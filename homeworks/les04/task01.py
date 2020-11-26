507b76d3c1eaa0e01d18607318bcd2a9ad77b3c9
Alex Klyanchin <k142@mail.ru>
2020-11-26 17:42:11 +0300

    les04 final

--- homeworks/les04/task01.py
+++ homeworks/les04/task01.py
@@ -13,15 +13,30 @@ homeworks/les02/task03.py
 
 3. Сформировать из введенного числа обратное по порядку входящих в него цифр и вывести на экран. Например,
 если введено число 3486, надо вывести 6843. """
+
+import cProfile
 import random
 
 
 def rev(func, n):
+    """
+    Вызов функции для тестирования и генерация случайного числа
+    :param func:
+    :param n: количество разрядов
+    :return:
+    """
     num = random.randint(10 ** (n - 1), 10 ** n)
     return num, func(num)
 
 
 def reverse1(n):
+    """
+    Преобразование числа в обратное по порядку входящих в него цифр.
+    Рекурсивный подход
+
+    :param n:
+    :return:
+    """
     nn = n % 10
     rank = n // 10
     if rank == 0:
@@ -31,6 +46,13 @@ def reverse1(n):
 
 
 def reverse2(n):
+    """
+    Преобразование числа в обратное по порядку входящих в него цифр.
+    Итерационный подход
+
+    :param n:
+    :return:
+    """
     result = ''
     while n > 0:
         result += str(n % 10)
@@ -39,23 +61,117 @@ def reverse2(n):
 
 
 def reverse3(n):
+    """
+    Преобразование числа в обратное по порядку входящих в него цифр.
+    Подход на основе срезов массивов
+
+    :param n:
+    :return:
+    """
     return str(n)[-1::-1]
 
 
-def test1(n):
-    rev(reverse1, n)
+def reverse4(n):
+    """
+    Преобразование числа в обратное по порядку входящих в него цифр.
+    Подход на основе преобразования в строку
+
+    :param n:
+    :return:
+    """
+    buf = str(n)
+    result = ''
+    for idx in range(len(buf), 0, -1):
+        result += buf[idx - 1]
+    return result
 
 
-def test2(n):
-    rev(reverse2, n)
+def test_reverse1(n):
+    return rev(reverse1, n)
 
 
-def test3(n):
-    rev(reverse3, n)
+def test_reverse2(n):
+    return rev(reverse2, n)
 
-# print(rev(reverse1, 10))
-# print(rev(reverse2, 10))
-# print(rev(reverse3, 10))
 
-# python -m timeit -n 100 -s "import task01" "task01.rev(reverse1, 10)"
-# python -m timeit -n 100 -s "import task01" "task01.test3(500)"
+def test_reverse3(n):
+    return rev(reverse3, n)
+
+
+def test_reverse4(n):
+    return rev(reverse4, n)
+
+
+# print(test_reverse1(500))
+# cProfile.run('test_reverse1(10)')
+# 21 function calls (12 primitive calls) in 0.000 seconds
+#      10/1    0.000    0.000    0.000    0.000 task01.py:32(reverse1)
+# cProfile.run('test_reverse1(100)')
+# 111 function calls (12 primitive calls) in 0.001 seconds
+#    100/1    0.001    0.000    0.001    0.001 task01.py:32(reverse1)
+# cProfile.run('test_reverse1(500)')
+# 511 function calls (12 primitive calls) in 0.003 seconds
+#     500/1    0.003    0.000    0.003    0.003 task01.py:32(reverse1)
+
+# python -m timeit -n 100 -s "import task01" "task01.test3(10)"
+
+# task01.test_reverse1(10)
+# 100 loops, best of 5: 7.88 usec per loop
+# task01.test_reverse1(100)
+# 100 loops, best of 5: 75.4 usec per loop
+# task01.test_reverse1(200)
+# 100 loops, best of 5: 219 usec per loop
+# task01.test_reverse1(500)
+# 100 loops, best of 5: 754 usec per loop
+# task01.test_reverse1(1000)
+# RecursionError: maximum recursion depth exceeded while getting the str of an object
+# task01.test_reverse1(1500)
+# RecursionError: maximum recursion depth exceeded while getting the str of an object
+
+# task01.test_reverse2(10)
+# 100 loops, best of 5: 7.09 usec per loop
+# task01.test_reverse2(100)
+# 100 loops, best of 5: 67.6 usec per loop
+# task01.test_reverse2(200)
+# 100 loops, best of 5: 156 usec per loop
+# task01.test_reverse2(500)
+# 100 loops, best of 5: 645 usec per loop
+# task01.test_reverse2(1000)
+# 100 loops, best of 5: 3.4 msec per loop
+# task01.test_reverse2(1500)
+# 100 loops, best of 5: 4.48 msec per loop
+
+# task01.test_reverse3(10)
+# 100 loops, best of 5: 3.29 usec per loop
+# task01.test_reverse3(100)
+# 100 loops, best of 5: 4.37 usec per loop
+# task01.test_reverse3(200)
+# 100 loops, best of 5: 5.52 usec per loop
+# task01.test_reverse3(500)
+# 100 loops, best of 5: 11.4 usec per loop
+# task01.test_reverse3(1000)
+# 100 loops, best of 5: 28.9 usec per loop
+# task01.test_reverse3(1500)
+# 100 loops, best of 5: 58.6 usec per loop
+
+# task01.test_reverse4(10)
+# 100 loops, best of 5: 4.89 usec per loop
+# task01.test_reverse4(100)
+# 100 loops, best of 5: 16.6 usec per loop
+# task01.test_reverse4(200)
+# 100 loops, best of 5: 30.6 usec per loop
+# task01.test_reverse4(500)
+# 100 loops, best of 5: 86.6 usec per loop
+# task01.test_reverse4(1000)
+# 100 loops, best of 5: 209 usec per loop
+# task01.test_reverse4(1500)
+# 100 loops, best of 5: 340 usec per loop
+
+# Выводы:
+# Реализовать алгоритм возможно различными путями. Преобразование числа является прямой реализацией
+# и наиболее затратной. При этом рекурсивный алгоритм(1) имеет еще ограничения по глубине вызовов.
+# Итерационный подход(2) имееет линейную сложность вычислений. Нахождение обратного 1500 разрядного числа
+# занимает около 4.48 милисек. Если использовать строчное предствление числа (4), то нахождение такого
+# числа занимает 340 микросек
+
+
--- homeworks/les04/task02.py
+++ homeworks/les04/task02.py
@@ -8,16 +8,97 @@
 Второй — без использования «Решета Эратосфена».
 Примечание. Вспомните классический способ проверки числа на простоту.
 Пример работы программ:
->>> sieve(2)
+sieve(2)
 3
->>> prime(4)
+prime(4)
 7
->>> sieve(5)
+sieve(5)
 11
->>> prime(1)
+prime(1)
 2
 Примечание по профилированию кода: для получения достоверных результатов при замере времени
 необходимо исключить/заменить функции print() и input() в анализируемом коде.
 
-С ними вы будете замерять время вывода данных в терминал и время, потраченное пользователем, на ввод данных, а не быстродействие самого алгоритма.
+С ними вы будете замерять время вывода данных в терминал и время, потраченное пользователем,
+на ввод данных, а не быстродействие самого алгоритма.
 """
+import cProfile
+
+
+def bolter(num):
+    n = 10 ** 6
+
+    def is_prime(n):
+        d = 2
+        while d * d <= n and n % d != 0:
+            d += 1
+        return d * d > n
+
+    count = 0
+    for elem in range(2, n):
+        if is_prime(elem):
+            count += 1
+            if count == num:
+                break
+    return elem
+
+
+def sieve(num):
+    n = 10 ** 6
+    si = [i for i in range(n)]
+    si[1] = 0
+    for i in range(2, n):
+        if si[i] != 0:
+            j = i * 2
+            while j < n:
+                si[j] = 0
+                j += i
+    count = 0
+    for elem in si:
+        if elem != 0:
+            count += 1
+            if count == num:
+                break
+    return elem
+
+
+
+# print(sieve(2))
+# print(sieve(4))
+# print(sieve(5))
+# print(sieve(1))
+#
+# print(bolter(2))
+# print(bolter(4))
+# print(bolter(5))
+# print(bolter(1))
+
+
+# print(sieve(100))
+
+
+# python -m timeit -n 100 -s "import task02" "task02.bolter(10)"
+
+# task02.bolter(10)
+# 100 loops, best of 5: 10.4 usec per loop
+# task02.bolter(100)
+# 100 loops, best of 5: 350 usec per loop
+# task02.bolter(500)
+# 100 loops, best of 5: 4.42 msec per loop
+# task02.bolter(1000)
+# 100 loops, best of 5: 11.9 msec per loop
+
+# task02.sieve(10)
+# 100 loops, best of 5: 298 usec per loop
+# task02.sieve(100)
+# 100 loops, best of 5: 375 usec per loop
+
+# cProfile.run('sieve(50)')
+#         1    1.063    1.063    1.353    1.353 task02.py:46(sieve)
+#         1    0.290    0.290    0.290    0.290 task02.py:48(<listcomp>)
+
+# Выводы: Метод прямого поиска простых чисел имеет квадратичную сложность. Метод «Решето Эратосфена» обладает такой
+# же сложнстью, однако больше подходит для поиска множества простых. Поэтому требует большего времени на подготовку
+# - генерацию "решета"
+
+
