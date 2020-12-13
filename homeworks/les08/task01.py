13e013886edfbb3b699edf0cf8e1eb0c7b2735e1
Alex Klyanchin <k142@mail.ru>
2020-12-14 00:51:18 +0300

    les08 task01

--- homeworks/les08/task01.py
+++ homeworks/les08/task01.py
@@ -3,3 +3,18 @@
 Сколько рукопожатий было?
 Примечание. Решите задачу при помощи построения графа.
 """
+import pprint
+
+n = int(input("Сколько друзей встретилось? "))
+
+hand_shake = [[0 for _ in range(n)] for _ in range(n)]
+
+for i in range(0, len(hand_shake)):
+    for j in range(i + 1, len(hand_shake[i])):
+        hand_shake[i][j] = 1
+
+pprint.pprint(hand_shake, width=500)
+
+s = sum([sum(el) for el in hand_shake])
+
+print("Всего рокопожатий: ", s)
