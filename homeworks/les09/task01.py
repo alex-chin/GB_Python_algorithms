5e1c740279c2e4cd82830f6209119e8dcbcd840b
Alex Klyanchin <k142@mail.ru>
2020-12-16 22:56:19 +0300

    les09 task01

--- homeworks/les09/task01.py
+++ homeworks/les09/task01.py
@@ -3,6 +3,35 @@
 Пусть на вход функции дана строка. Требуется вернуть количество различных подстрок в этой строке.
 Примечания:
 * в сумму не включаем пустую строку и строку целиком;
-* без использования функций для вычисления хэша (hash(), sha1() или любой другой из модуля hashlib
+    * без использования функций для вычисления хэша (hash(), sha1() или любой другой из модуля hashlib
 задача считается не решённой.
 """
+
+
+def permutation(buf):
+    """ генератор всех подстрок строки и возврат хэш подстрок
+    :param buf:
+    """
+    length = len(buf)
+    for l_len in range(1, length - 1):
+        for idx in range(length):
+            if l_len + idx > length:
+                break
+            yield hash(buf[idx:(idx + l_len)])
+
+
+def substring_unique(buf):
+    """ последовательный анализ хэшей на уникальность и выдача длины списка уникальных
+    :param buf:
+    :return:
+    """
+    s_unique = []
+    for tag in permutation(buf):
+        if tag not in s_unique:
+            s_unique.append(tag)
+    return len(s_unique)
+
+
+buf = input('Введите строку :')
+print('Количество всех подстрок строки', len(list(permutation(buf))))
+print('Количество различных подстрок строки', substring_unique(buf))
--- homeworks/les09/task02.py
+++ homeworks/les09/task02.py
@@ -1,3 +1,20 @@
 """
 2. Закодируйте любую строку по алгоритму Хаффмана.
 """
+from collections import Counter
+
+
+class Node:
+    def __init__(self, value, freq, left, right):
+        self.value = value
+        self.freq = freq
+        self.left = left
+        self.right = right
+
+
+def build_tree(buf):
+    freq = dict(sorted(Counter(buf).items(), key=lambda item: item[1], reverse=True))
+    return freq
+
+
+print(build_tree('laklajlkas jdlk jiouroew iuri ows euowieru'))
